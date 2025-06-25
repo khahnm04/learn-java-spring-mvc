f6a83333f0532a77cd1416b3d797af68ecf6dce0
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-06-25 09:45:38 +0700

    #33. Spring Boot Devtool

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -9,10 +9,7 @@ public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext hoidanit = SpringApplication.run(LaptopshopApplication.class, args);
-		for (String s : hoidanit.getBeanDefinitionNames()) {
-			System.out.println(s);
-		}
+		SpringApplication.run(LaptopshopApplication.class, args);
 
 	}
 
