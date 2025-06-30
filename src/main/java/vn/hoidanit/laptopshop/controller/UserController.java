278ace12a023bc041fd2f5855048dafbd9bec54b
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-06-30 09:28:58 +0700

    #63. Chữa bài tập design table users

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -36,13 +36,18 @@ public class UserController {
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        return "/admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "/admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-        System.out.println("run here" + hoidanit);
+        System.out.println("run here " + hoidanit);
         this.userService.handleSaveUser(hoidanit);
         return "hello";
     }
