949087132e29622fbbefda3d0c490fd6286b8591
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-07-03 07:38:47 +0700

    #76. Mô hình hóa với Spring Data JPA

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -17,6 +19,7 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+    private String avatar;
 
     public long getId() {
         return id;
@@ -66,10 +69,18 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
 }
